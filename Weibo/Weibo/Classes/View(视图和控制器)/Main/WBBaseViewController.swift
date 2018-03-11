a733d83a8edb68958e1b5efb41bc02aa088aea24
mandy04 <ma_chun_hui@163.com>
2018-03-11 15:20:12 +0800

    添加一层底层视图，矫正导航视图上按钮位置

--- Weibo/Weibo/Classes/View(视图和控制器)/Main/WBBaseViewController.swift
+++ Weibo/Weibo/Classes/View(视图和控制器)/Main/WBBaseViewController.swift
@@ -5,13 +5,19 @@
 //  Created by Smile on 2018/3/5.
 //  Copyright © 2018年 llbt. All rights reserved.
 //
+/*基类：1.所有主控制器的基类
+ *     2.设置界面  设置新的导航条+navItem  、 重写title属性，保证外部设置title的代码不需要任何的变化
+ *     3.设置表格视图
+ *     4.封住刷新逻辑 上拉+下拉
+ *     5.访客试图  用户没有登录的界面显示
+ */
 
 import UIKit
 
 class WBBaseViewController: UIViewController {
 
     //设置导航条
-    lazy var navigationBar = UINavigationBar.init(frame: CGRect(x: 0, y: 0, width: UIScreen.cz_screenWidth(), height: 64))
+    lazy var navigationBar = UINavigationBar.init(frame: CGRect(x: 0, y: 20, width: UIScreen.cz_screenWidth(), height: 64))
     //设置导航条目
     lazy var navItem = UINavigationItem()
     
@@ -20,7 +26,7 @@ class WBBaseViewController: UIViewController {
         setupUI()
     }
     
-    //重写title setter
+    //重写title setter方法，设置标题
     override var title: String? {
         didSet {
             navItem.title = title
@@ -37,8 +43,13 @@ extension WBBaseViewController {
     
     view.backgroundColor = UIColor.cz_random()
 
+    //FIXME: 添加一个底层视图，矫正title显示位置
+    let fixView = UIView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.cz_screenWidth(), height: 64))
+    fixView.backgroundColor = UIColor.cz_color(withHex: 0xF6F6F6)
+    view.addSubview(fixView)
+
     //添加navigationBar
-    view.addSubview(navigationBar)
+    fixView.addSubview(navigationBar)
     //将item设置给bar
     navigationBar.items = [navItem]
     //设置navigationBar 条子渲染颜色

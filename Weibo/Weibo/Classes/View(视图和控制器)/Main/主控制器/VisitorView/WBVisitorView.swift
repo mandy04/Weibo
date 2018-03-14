2d916adb5c122e8d25ab0ca67c335869b117d7e6
mandy04 <ma_chun_hui@163.com>
2018-03-15 05:16:10 +0800

    访客试图添加控件，并使用苹果原生自动布局

--- Weibo/Weibo/Classes/View(视图和控制器)/Main/主控制器/VisitorView/WBVisitorView.swift
+++ Weibo/Weibo/Classes/View(视图和控制器)/Main/主控制器/VisitorView/WBVisitorView.swift
@@ -20,12 +20,12 @@ class WBVisitorView: UIView {
     }
     
     //MARK: - 私有控件  懒加载属性只有调用 UIKit 控件的指定构造函数，其他都需要使用类型
-    ///icon
-    private lazy var icoImage:UIImageView = UIImageView.init(image: UIImage.init(named: "visitordiscover_feed_image_smallicon"))
-    ///hourseICon
-    private lazy var houseIcoImage:UIImageView = UIImageView.init(image: UIImage.init(named: "visitordiscover_feed_image_house"))
-    ///label
-    private lazy var textLabel:UILabel = UILabel.cz_label(withText: "000", fontSize: 14, color: UIColor.darkGray)
+    ///icon 图标
+    private lazy var iconImage:UIImageView = UIImageView.init(image: UIImage.init(named: "visitordiscover_feed_image_smallicon"))
+    ///hourseICon 小房子
+    private lazy var houseIconImage:UIImageView = UIImageView.init(image: UIImage.init(named: "visitordiscover_feed_image_house"))
+    ///tipLabel 提示标签
+    private lazy var tipLabel:UILabel = UILabel.cz_label(withText: "000", fontSize: 14, color: UIColor.darkGray)
     ///注册
     private lazy var registerButton:UIButton = UIButton.cz_textButton("注册",
                                                                       fontSize: 14,
@@ -44,6 +44,57 @@ class WBVisitorView: UIView {
 extension WBVisitorView {
     func setupUI() {
         backgroundColor = UIColor.white
+        //1. 添加视图
+        addSubview(iconImage)
+        addSubview(houseIconImage)
+        addSubview(tipLabel)
+        addSubview(registerButton)
+        addSubview(loginButton)
+        
+        //2. 使用原生autolayout， 不可与autoreresize同时存在
+        for v in subviews {
+            v.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        //3. 自动布局 "view1.attr1 = view2.attr2 * multiplier + constant"
+        addConstraints([NSLayoutConstraint.init(item: iconImage,
+                                                attribute: .centerX,
+                                                relatedBy: .equal,
+                                                toItem: self,
+                                                attribute: .centerX,
+                                                multiplier: 1.0,
+                                                constant: 0)])
+        addConstraints([NSLayoutConstraint.init(item: iconImage,
+                                                attribute: .centerY,
+                                                relatedBy: .equal,
+                                                toItem: self,
+                                                attribute: .centerY,
+                                                multiplier: 1.0,
+                                                constant: 0)])
+        
+        addConstraints([NSLayoutConstraint.init(item: houseIconImage,
+                                                attribute: .centerX,
+                                                relatedBy: .equal,
+                                                toItem: iconImage,
+                                                attribute: .centerX,
+                                                multiplier: 1.0,
+                                                constant: 1)])
+        addConstraints([NSLayoutConstraint.init(item: houseIconImage,
+                                                attribute: .centerY,
+                                                relatedBy: .equal,
+                                                toItem: iconImage,
+                                                attribute: .centerY,
+                                                multiplier: 1.0,
+                                                constant: 1)])
+        addConstraints([NSLayoutConstraint.init(item: tipLabel,
+                                                attribute: .height,
+                                                relatedBy: .equal,
+                                                toItem: iconImage,
+                                                attribute: .height,
+                                                multiplier: 1.0,
+                                                constant: 60)])
+        
+
         
     }
 }

c9f80695f9e49b8638724ec1448d83cafdf05e6d
mandy04 <ma_chun_hui@163.com>
2018-08-16 16:47:53 +0800

    数据存储方式总结归纳

--- Weibo/Weibo/Classes/Tools(工具)/Network/Model/WBUserAccount.swift
+++ Weibo/Weibo/Classes/Tools(工具)/Network/Model/WBUserAccount.swift
@@ -15,8 +15,8 @@ class WBUserAccount: NSObject {
     var access_Token: String?  //= "2.00fXeqaFRXwdnC58ac5de911LgkdlC"
     //用户代号
     var uid: String?
-    //开发者 5年
-    //使用者 3天  access_token的生命周期，单位是秒数。
+    //开发者 5年 ，每次登录之后，都是5年
+    //使用者 3天  access_token的生命周期，单位是秒数。  会从第一次登录后开始递减
     var expires_in: TimeInterval = 0 {
         didSet{
             expiresDate = Date(timeIntervalSinceNow: expires_in) as NSDate
@@ -28,5 +28,13 @@ class WBUserAccount: NSObject {
     
     override var description: String {
         return yy_modelDescription()
+    }
+    /**1. 偏好设置（存小）
+     * 2. 沙盒 -归档/json/plist
+     * 3. 数据库  FMDB/CoreData
+     * 4. 钥匙串访问（存小/自动加密 -需要使用框架SSKeyChain）
+     */
+    func saveAccount() {
+        
     }
 }
